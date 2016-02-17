9be7848fa50cb4b39d1a2e7d1ce7444f75dbec6e
John Fastabend <john.fastabend@gmail.com>
2016-02-16 21:17:09 -0800

    net: sched: add cls_u32 offload hooks for netdevs
    This patch allows netdev drivers to consume cls_u32 offloads via
    the ndo_setup_tc ndo op.
    
    This works aligns with how network drivers have been doing qdisc
    offloads for mqprio.
    
    Signed-off-by: John Fastabend <john.r.fastabend@intel.com>
    Acked-by: Jiri Pirko <jiri@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -823,17 +823,21 @@ struct netdev_xdp {
 	};
 };
 
-/* This structure holds attributes of qdisc and classifiers
+/* These structures hold the attributes of qdisc and classifiers
  * that are being passed to the netdevice through the setup_tc op.
  */
 enum {
 	TC_SETUP_MQPRIO,
+	TC_SETUP_CLSU32,
 };
+
+struct tc_cls_u32_offload;
 
 struct tc_to_netdev {
 	unsigned int type;
 	union {
 		u8 tc;
+		struct tc_cls_u32_offload *cls_u32;
 	};
 };
 
