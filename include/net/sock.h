b92adb940ed2d1a488ea2f5fb70af58b940a6fa2
Andrey Ryabinin <aryabinin@virtuozzo.com>
2016-05-18 19:19:27 +0300

    net: sock: move ->sk_shutdown out of bitfields.
    ->sk_shutdown bits share one bitfield with some other bits in sock struct,
    such as ->sk_no_check_[r,t]x, ->sk_userlocks ...
    sock_setsockopt() may write to these bits, while holding the socket lock.
    
    In case of AF_UNIX sockets, we change ->sk_shutdown bits while holding only
    unix_state_lock(). So concurrent setsockopt() and shutdown() may lead
    to corrupting these bits.
    
    Fix this by moving ->sk_shutdown bits out of bitfield into a separate byte.
    This will not change the 'struct sock' size since ->sk_shutdown moved into
    previously unused 16-bit hole.
    
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Suggested-by: Hannes Frederic Sowa <hannes@stressinduktion.org>
    Acked-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/sock.h
+++ include/net/sock.h
@@ -420,7 +420,7 @@ struct sock {
 #endif
 
 	kmemcheck_bitfield_begin(flags);
-	unsigned int		sk_shutdown  : 2,
+	unsigned int		sk_padding : 2,
 				sk_no_check_tx : 1,
 				sk_no_check_rx : 1,
 				sk_userlocks : 4,
@@ -428,6 +428,7 @@ struct sock {
 				sk_type      : 16;
 #define SK_PROTOCOL_MAX U8_MAX
 	kmemcheck_bitfield_end(flags);
+
 	int			sk_wmem_queued;
 	gfp_t			sk_allocation;
 	u32			sk_pacing_rate; /* bytes per second */
@@ -460,6 +461,7 @@ struct sock {
 	seqlock_t		sk_stamp_seq;
 #endif
 	u16			sk_tsflags;
+	u8			sk_shutdown;
 	u32			sk_tskey;
 	struct socket		*sk_socket;
 	void			*sk_user_data;
