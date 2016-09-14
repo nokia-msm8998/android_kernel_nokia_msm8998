2e4e859d60cdb068e702f542238f6f571ae02ba8
Deepa Dinamani <deepa.kernel@gmail.com>
2016-09-14 07:48:02 -0700

    vfs: Add current_time() api
    current_fs_time() is used for inode timestamps.
    
    Change the signature of the function to take inode pointer
    instead of superblock as per Linus's suggestion.
    
    Also, move the api under vfs as per the discussion on the
    thread: https://lkml.org/lkml/2016/6/9/36 . As per Arnd's
    suggestion on the thread, changing the function name.
    
    current_fs_time() will be deleted after all the references
    to it are replaced by current_time().
    
    There was a bug reported by kbuild test bot with the change
    as some of the calls to current_time() were made before the
    super_block was initialized. Catch these accidental assignments
    as timespec_trunc() does for wrong granularities. This allows
    for the function to work right even in these circumstances.
    But, adds a warning to make the user aware of the bug.
    
    A coccinelle script was used to identify all the current
    .alloc_inode super_block callbacks that updated inode timestamps.
    proc filesystem was the only one that was modifying inode times
    as part of this callback. The series includes a patch to fix that.
    
    Note that timespec_trunc() will also be moved to fs/inode.c
    in a separate patch when this will need to be revamped for
    bounds checking purposes.
    
    Signed-off-by: Deepa Dinamani <deepa.kernel@gmail.com>
    Reviewed-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1472,6 +1472,8 @@ struct super_block {
 
 extern struct timespec current_fs_time(struct super_block *sb);
 
+extern struct timespec current_time(struct inode *inode);
+
 /*
  * Snapshotting support.
  */
