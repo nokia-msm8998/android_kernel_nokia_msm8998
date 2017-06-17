7cf29a593487a97c6b74926291f7d731c4402aa3
Tejun Heo <tj@kernel.org>
2017-06-17 08:10:08 -0400

    sched: Allow migrating kthreads into online but inactive CPUs
    Per-cpu workqueues have been tripping CPU affinity sanity checks while
    a CPU is being offlined.  A per-cpu kworker ends up running on a CPU
    which isn't its target CPU while the CPU is online but inactive.
    
    While the scheduler allows kthreads to wake up on an online but
    inactive CPU, it doesn't allow a running kthread to be migrated to
    such a CPU, which leads to an odd situation where setting affinity on
    a sleeping and running kthread leads to different results.
    
    Each mem-reclaim workqueue has one rescuer which guarantees forward
    progress and the rescuer needs to bind itself to the CPU which needs
    help in making forward progress; however, due to the above issue,
    while set_cpus_allowed_ptr() succeeds, the rescuer doesn't end up on
    the correct CPU if the CPU is in the process of going offline,
    tripping the sanity check and executing the work item on the wrong
    CPU.
    
    This patch updates __migrate_task() so that kthreads can be migrated
    into an inactive but online CPU.
    
    Change-Id: I38cc3eb3b2ec5b7034cc72a2bcdd32a549314915
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Reported-by: "Paul E. McKenney" <paulmck@linux.vnet.ibm.com>
    Reported-by: Steven Rostedt <rostedt@goodmis.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1179,6 +1179,14 @@ static struct rq *__migrate_task(struct rq *rq, struct task_struct *p, int dest_
 {
 	int src_cpu;
 
+	if (p->flags & PF_KTHREAD) {
+		if (unlikely(!cpu_online(dest_cpu)))
+			return ret;
+	} else {
+		if (unlikely(!cpu_active(dest_cpu)))
+			return ret;
+	}
+
 	/* Affinity changed (again). */
 	if (!is_cpu_allowed(p, dest_cpu))
 		return rq;
