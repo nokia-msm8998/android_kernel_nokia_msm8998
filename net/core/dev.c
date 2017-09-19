acca1ec972ad70caf5e7282a3e9a0347653b7f5f
Edward Cree <ecree@solarflare.com>
2017-09-19 18:45:56 +0100

    net: change skb->mac_header when Generic XDP calls adjust_head
    Since XDP's view of the packet includes the MAC header, moving the start-
     of-packet with bpf_xdp_adjust_head needs to also update the offset of the
     MAC header (which is relative to skb->head, not to the skb->data that was
     changed).
    Without this, tcpdump sees packets starting from the old MAC header rather
     than the new one, at least in my tests on the loopback device.
    
    Fixes: b5cdae3291f7 ("net: Generic XDP")
    Signed-off-by: Edward Cree <ecree@solarflare.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -3641,6 +3641,7 @@ static u32 netif_receive_generic_xdp(struct sk_buff *skb,
 		__skb_pull(skb, off);
 	else if (off < 0)
 		__skb_push(skb, -off);
+	skb->mac_header += off;
 
 	switch (act) {
 	case XDP_REDIRECT:
