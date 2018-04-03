0c50c07db93a79b35a29e7ff060f6b56d929cb4f
Anders Roxell <anders.roxell@linaro.org>
2018-04-03 14:09:47 +0200

    kernel/bpf/syscall: fix warning defined but not used
    There will be a build warning -Wunused-function if CONFIG_CGROUP_BPF
    isn't defined, since the only user is inside #ifdef CONFIG_CGROUP_BPF:
    kernel/bpf/syscall.c:1229:12: warning: ‘bpf_prog_attach_check_attach_type’
        defined but not used [-Wunused-function]
     static int bpf_prog_attach_check_attach_type(const struct bpf_prog *prog,
                ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    Current code moves function bpf_prog_attach_check_attach_type inside
    ifdef CONFIG_CGROUP_BPF.
    
    Fixes: 5e43f899b03a ("bpf: Check attach type at prog load time")
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -1214,18 +1214,6 @@ bpf_prog_load_check_attach_type(enum bpf_prog_type prog_type,
 	}
 }
 
-static int bpf_prog_attach_check_attach_type(const struct bpf_prog *prog,
-					     enum bpf_attach_type attach_type)
-{
-	switch (prog->type) {
-	case BPF_PROG_TYPE_CGROUP_SOCK:
-	case BPF_PROG_TYPE_CGROUP_SOCK_ADDR:
-		return attach_type == prog->expected_attach_type ? 0 : -EINVAL;
-	default:
-		return 0;
-	}
-}
-
 struct bpf_prog *bpf_prog_get_type_dev(u32 ufd, enum bpf_prog_type type,
 				       bool attach_drv)
 {
@@ -1385,6 +1373,18 @@ static int bpf_obj_get(const union bpf_attr *attr)
 }
 
 #ifdef CONFIG_CGROUP_BPF
+
+static int bpf_prog_attach_check_attach_type(const struct bpf_prog *prog,
+					     enum bpf_attach_type attach_type)
+{
+	switch (prog->type) {
+	case BPF_PROG_TYPE_CGROUP_SOCK:
+	case BPF_PROG_TYPE_CGROUP_SOCK_ADDR:
+		return attach_type == prog->expected_attach_type ? 0 : -EINVAL;
+	default:
+		return 0;
+	}
+}
 
 #define BPF_PROG_ATTACH_LAST_FIELD attach_flags
 
