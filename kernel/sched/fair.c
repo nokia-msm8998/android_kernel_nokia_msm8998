1457212f13d6cbc9a7613e7475aa43e9d390b44a
Pavankumar Kondeti <pkondeti@codeaurora.org>
2019-03-08 10:04:40 +0530

    sched/fair: Don't let tasks slip away from gold to silver cluster
    We often see a BIG/medium task being migrated from gold cluster to
    silver cluster during load balance when the gold CPU has 2 tasks
    including this BIG task. For example, BIG task wakes up a binder
    and binder preempts and running. At this time, the silver CPU pull
    the BIG task since the overall utilization is more. The gold CPU
    becomes idle immediately and may not be able to pull the medium
    task. It can also happen when a pinned kworker runs on the gold
    CPU.
    
    Change-Id: I7d8caad6978bfa93aa3cdd772400e807b9f15e3d
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -131,6 +131,8 @@ unsigned int sysctl_sched_cfs_bandwidth_slice = 5000UL;
  */
 unsigned int capacity_margin = 1280; /* ~20% */
 
+static unsigned int __maybe_unused sched_small_task_threshold = 102;
+
 static inline void update_load_add(struct load_weight *lw, unsigned long inc)
 {
 	lw->weight += inc;
@@ -9633,7 +9635,10 @@ static struct rq *find_busiest_queue(struct lb_env *env,
 		 * Ignore cpu, which is undergoing active_balance and doesn't
 		 * have more than 2 tasks.
 		 */
-		if (rq->active_balance && rq->nr_running <= 2)
+		if (env->sd->flags & SD_ASYM_CPUCAPACITY &&
+		    capacity_of(env->dst_cpu) < capacity &&
+		    (rq->nr_running == 1 || (rq->nr_running == 2 &&
+		     task_util(rq->curr) < sched_small_task_threshold)))
 			continue;
 
 		capacity = capacity_of(i);
