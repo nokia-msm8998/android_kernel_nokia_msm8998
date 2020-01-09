fba78984bce6fbd55af2f77c11fdee3b5ce3c0ca
Mat Martineau <mathew.j.martineau@linux.intel.com>
2020-01-09 07:59:18 -0800

    tcp, ulp: Add clone operation to tcp_ulp_ops
    If ULP is used on a listening socket, icsk_ulp_ops and icsk_ulp_data are
    copied when the listener is cloned. Sometimes the clone is immediately
    deleted, which will invoke the release op on the clone and likely
    corrupt the listening socket's icsk_ulp_data.
    
    The clone operation is invoked immediately after the clone is copied and
    gives the ULP type an opportunity to set up the clone socket and its
    icsk_ulp_data.
    
    The MPTCP ULP clone will silently fallback to plain TCP on allocation
    failure, so 'clone()' does not need to return an error code.
    
    v6 -> v7:
     - move and rename ulp clone helper to make it inline-friendly
    v5 -> v6:
     - clarified MPTCP clone usage in commit message
    
    Signed-off-by: Mat Martineau <mathew.j.martineau@linux.intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -2237,6 +2237,9 @@ struct tcp_ulp_ops {
 	int (*init)(struct sock *sk);
 	/* cleanup ulp */
 	void (*release)(struct sock *sk);
+	/* clone ulp */
+	void (*clone)(const struct request_sock *req, struct sock *newsk,
+		      const gfp_t priority);
 
 	int		uid;
 	char		name[TCP_ULP_NAME_MAX];
