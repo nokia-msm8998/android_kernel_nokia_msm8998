bc273d00bd0b0de5f0cde498012d4b82da505601
Georg Veichtlbauer <georg@vware.at>
2023-06-07 14:18:55 +0200

    Revert "msm: camera: isp: Handle frame drop due to delay"
    Camera HAL has troubles dealing with frame drop notifs.
    Revert 9/9
    
    This reverts commit 39e7df335515c3f11859901d650d47ee983fcf6a.
    
    Change-Id: I29be923bf5ea76aa0b1184adc664af1b9de7dd8f

--- drivers/media/platform/msm/camera_v2/isp/msm_isp.h
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp.h
@@ -852,7 +852,7 @@ struct vfe_device {
 	uint32_t recovery_irq1_mask;
 	/* total bandwidth per vfe */
 	uint64_t total_bandwidth;
-	struct isp_kstate *isp_page;
+	struct isp_proc *isp_page;
 };
 
 struct vfe_parent_device {
